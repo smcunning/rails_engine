850629c30a69e4368efaa9c12da8fedb7f3d1f19
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-12 14:15:07 -0700

    Add merchant_by_id method in facade

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -2,4 +2,8 @@ class MerchantFacade
   def self.all_merchants
     MerchantSerializer.new(Merchant.all)
   end
+
+  def self.merchant_by_id(id)
+    MerchantSerializer.new(Merchant.find(id))
+  end
 end
