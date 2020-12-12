f778f75c02b9a8adbeb72daf609f369889a240b7
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-12 00:01:05 -0700

    Update spec for merchants index api test

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,15 +8,12 @@ describe 'Merchants API' do
 
     expect(response).to be_successful
     merchants = JSON.parse(response.body, symbolize_names: true)
-
+    merchants = merchants[:data]
     expect(merchants.count).to eq(3)
 
     merchants.each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_a Integer
-
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a String
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a String
     end
   end
 end
