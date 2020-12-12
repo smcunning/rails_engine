42cac30a5605561dacebf7468769d3298b2732b8
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-11 18:16:18 -0700

    Create customer model, factory and spec

---
+++ app/models/customer.rb
@@ -0,0 +1,8 @@
+class Customer < ApplicationRecord
+  has_many :invoices
+  has_many :merchants, :through => :invoices
+  has_many :transactions, :through => :invoices
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
