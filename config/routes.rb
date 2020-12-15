5746fb16741f38bd2cb0d2ca0f0fd705960b6fc8
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 17:08:06 -0700

    Add namespaced merchant route to revenue controller and most_revenue action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/most_revenue', to: 'revenue#most_revenue'
+      end
+
       resources :merchants do
         resources :items, only: [:index], module: :merchants
       end
