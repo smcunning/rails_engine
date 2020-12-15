9681860e7ca5da295e7ef387e00c0cf672dc8720
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 17:48:56 -0700

    Add test for most_revenue method in merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,4 +10,39 @@ RSpec.describe Merchant, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name) }
   end
+
+  describe "instance methods" do
+    it "#most_revenue" do
+      merchant_1 = create(:merchant)
+      merchant_2 = create(:merchant)
+      merchant_3 = create(:merchant)
+
+      item_1 = create(:item, unit_price: 10.00, merchant: merchant_1)
+      item_2 = create(:item, unit_price: 15.00, merchant: merchant_2)
+      item_3 = create(:item, unit_price: 5.00, merchant: merchant_3)
+
+      5.times { create(:invoice, merchant: merchant_1) }
+      5.times { create(:invoice, merchant: merchant_2) }
+      5.times { create(:invoice, merchant: merchant_3) }
+
+      merchant_1.invoices.each do |invoice|
+        create(:invoice_item, invoice: invoice, item: item_1, quantity: 100)
+        create(:transaction, invoice: invoice)
+      end
+
+      merchant_2.invoices.each do |invoice|
+        create(:invoice_item, invoice: invoice, item: item_2, quantity: 10)
+        create(:transaction, invoice: invoice)
+      end
+
+      merchant_3.invoices.each do |invoice|
+        create(:invoice_item, invoice: invoice, item: item_3, quantity: 1000)
+        create(:transaction, invoice: invoice)
+      end
+
+      merchants = Merchant.most_revenue(2)
+      expect(merchants).to be_an Object
+      expect(merchants).to include(merchant_3, merchant_1)
+    end
+  end
 end
