4b2d8fe1b2b06c5a547042e29d836669741f8377
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-14 17:49:24 -0700

    Update revenue controller most_revenue action and add most_revenue method to merchant model

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def most_revenue
+    render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,13 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates :name, presence: true
+
+  def self.most_revenue(quantity)
+    Merchant.select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
+            .joins(invoices: [:invoice_items, :transactions])
+            .group(:id)
+            .order("total_revenue desc")
+            .limit(quantity)
+  end
+
 end
