52a1383bf34aa4e4a4fb1750ff1b16db31106879
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-15 17:32:57 -0700

    Dream driving the show action for find merchant

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
+    name_query = params[:name]
+    render json: MerchantSerializer.new(Merchant.find_merchant(name_query))
   end
 end
