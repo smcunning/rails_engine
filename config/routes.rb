2cd3d896c605a82758b236865724999e25d41ee0
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-16 16:31:01 -0700

    Add /items/find path to search controller, show action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
       end
 
+      namespace :items do
+        get '/find', to: 'search#show'
+      end
+
       resources :merchants do
         resources :items, only: [:index], module: :merchants
       end
