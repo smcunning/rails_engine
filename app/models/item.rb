a78ec8e2562bf337959c181813713f3584dfeb7b
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-16 21:04:35 -0700

    Add find_all method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,14 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :unit_price, presence: true
 
+  def self.find_all_items(attribute, query)
+    if attribute == "created_at" || attribute == "updated_at"
+      where("DATE(#{attribute}) = ?", "%#{query}%")
+    else
+      where("#{attribute} ILIKE ?", "%#{query}%")
+    end
+  end
+
   def self.find_item(attribute, query)
     if attribute == "created_at" || attribute == "updated_at"
       find_by("DATE(#{attribute}) = ?", "%#{query}%")
