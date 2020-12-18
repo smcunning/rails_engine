5f074214aab0ed229e6e960bc989be8d95699d82
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-17 19:31:59 -0700

    Create a route to get merchants/most_items to merchants items controller #most_items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :merchants do
         get '/most_revenue', to: 'revenue#most_revenue'
+        get '/most_items', to: 'items#most_items'
         get '/:id/revenue', to: 'revenue#revenue'
         get '/find_all', to: 'search#index'
         get '/find', to: 'search#show'
