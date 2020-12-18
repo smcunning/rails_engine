180b844639ffb1f28667883e7393c0a2dc20d639
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-17 17:23:08 -0700

    Add revenue action in merchant revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
+  def revenue
+    render json: RevenueSerializer.new(RevenueFacade.new.merchant_revenue(params[:id]))
+  end
+
   def most_revenue
     render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
   end
