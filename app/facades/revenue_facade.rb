c950b0a8611d38b7be26e8561e2a9cd790307c27
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-12-17 21:39:05 -0700

    Add a revenue facade

--- app/facades/revenue_facade.rb
+++ app/facades/revenue_facade.rb
@@ -4,4 +4,9 @@ class RevenueFacade
     revenue = merchant.get_revenue
     Revenue.new(revenue)
   end
+
+  def self.total_revenue_by_date(start, stop)
+    revenue = Invoice.total_revenue_by_date(start, stop)
+    Revenue.new(revenue)
+  end
 end
